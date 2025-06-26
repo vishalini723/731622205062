fd04906131b0036f323a8b7af36621ba36ef6c1f
vishalini723 <vishavishalini723@gmail.com>
2025-06-26 17:58:21 +0530

    home.js

--- urlshort/src/pages/home.jsx
+++ urlshort/src/pages/home.jsx
@@ -1,26 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Log } from '../utils/log';
+import React from 'react';
+import UrlShortenerForm from '../components/form';
 
-export default function StatsPage() {
-  const [urls, setUrls] = useState([]);
-
-  useEffect(() => {
-    const all = Object.keys(localStorage).map(key => JSON.parse(localStorage.getItem(key)));
-    setUrls(all);
-    Log("frontend", "info", "page", "Visited stats page");
-  }, []);
-
+export default function HomePage() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h2>URL Stats</h2>
-      {urls.length === 0 ? <p>No data found</p> : urls.map((url, idx) => (
-        <div key={idx}>
-          <p><b>Original:</b> {url.original}</p>
-          <p><b>Short:</b> {url.short}</p>
-          <p><b>Expiry:</b> {url.expiry}</p>
-          <hr />
-        </div>
-      ))}
+      <h2>URL Shortener</h2>
+      <UrlShortenerForm />
     </div>
   );
 }
